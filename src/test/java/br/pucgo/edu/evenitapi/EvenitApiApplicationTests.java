38c3e7a83d7f3a202763bd665a0bce55bbf6ed2f
pablohsz <pablosouza.dev@gmail.com>
2023-11-24 21:04:36 -0300

    initial commit

---
+++ src/test/java/br/pucgo/edu/evenitapi/EvenitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucgo.edu.evenitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
