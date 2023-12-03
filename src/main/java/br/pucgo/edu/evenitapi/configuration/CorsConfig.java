014396ef596a8871409fc891b5037279d55d0396
pablohsz <pablosouza.dev@gmail.com>
2023-12-03 10:51:06 -0300

    allowing patch requests

---
+++ src/main/java/br/pucgo/edu/evenitapi/configuration/CorsConfig.java
@@ -0,0 +1,33 @@
+package br.pucgo.edu.evenitapi.configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String reportPath = uploadPath("./generated-reports");
+
+        registry.addResourceHandler("/generated-reports/**")
+                .addResourceLocations("file:/"+reportPath+"/");
+    }
+
+    private String uploadPath(String directory){
+        Path uploadDirPath = Paths.get(directory);
+        return uploadDirPath.toFile().getAbsolutePath();
+    }
+}
